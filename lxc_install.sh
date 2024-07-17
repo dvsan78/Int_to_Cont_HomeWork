580211a030ba79d2e10b6f570c3e1ad29dd705db
d.sanchischin <d.sanchischin@ase-ec.ru>
2024-07-17 22:45:36 +0300

    comm

--- lxc_install.sh
+++ lxc_install.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-echo “Updating system and installing dependences”
-apt-get update 
-apt-get upgrade -y  
-apt-get install lxc lxc-astra
-echo “Create conteiner name myapp”
-lxc-create -t astralinux-se -n myapp
-lxc-start myapp
-lxc-attach myapp
-apt-get update
-apt install nginx php-fpm
+#echo “Updating system and installing dependences”
+#apt-get update 
+#apt-get upgrade -y  
+#apt-get install lxc lxc-astra
+#echo “Create conteiner name myapp”
+#lxc-create -t astralinux-se -n myapp
+#lxc-start myapp
+#lxc-attach myapp
+sudo lxc-attach -n astra apt-get update
+
+#apt-get update
+#apt install nginx php-fpm
