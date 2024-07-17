e2f3c3ddbd86a19cf22ce3195fb3c65b409c18ce
d.sanchischin <d.sanchischin@ase-ec.ru>
2024-07-17 20:51:45 +0300

    commit

--- lxc_install.sh
+++ lxc_install.sh
@@ -4,4 +4,4 @@ apt-get update > /dev/null 2>&1
 apt-get upgrade -y > /dev/null 2>&1
 apt-get install lxc lxc-astra
 echo “Create conteiner name $1”
-lxc-create -t astralinux-se  -n $1
+sudo lxc-create -t ubuntu -n $1
